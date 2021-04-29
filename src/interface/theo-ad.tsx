a2c29f6e7c7f822d2af20a0fb3aea8e0766067f2
theo <me@t3.gg>
2021-04-28 23:25:21 -0700

    Styling

--- src/interface/theo-ad.tsx
+++ src/interface/theo-ad.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 export const AdForTheo = () => (
-  <div style={{ position: "absolute", right: 0, top: 0, padding: 10 }}>
+  <div
+    style={{
+      position: "absolute",
+      right: -5,
+      top: -5,
+      padding: 10,
+      backgroundColor: "white",
+      borderBottomLeftRadius: 10,
+      border: "2px solid black",
+    }}
+  >
     Dumb idea made quick by <a href="https://twitter.com/t3dotgg">Theo</a>
   </div>
 );
