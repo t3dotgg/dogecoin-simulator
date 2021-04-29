e6bbd8addd227b8b3cbe477b96c7a640e3b9e01e
theo <me@t3.gg>
2021-04-29 16:26:51 -0700

    Add basic tracking

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import Game from "./Game";
 
+import ReactGA from "react-ga";
+ReactGA.initialize("UA-25660758-13");
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 ReactDOM.render(
   <React.StrictMode>
     <Game />
