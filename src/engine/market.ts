3fddc247245ee1fce5769f84b456b10662363662
theo <me@t3.gg>
2021-04-30 01:24:52 -0700

    Locally store last price

--- src/engine/market.ts
+++ src/engine/market.ts
@@ -1,5 +1,7 @@
 import create from "zustand";
 
+const LAST_PRICE_KEY = "last-price";
+
 type MarketState = {
   dogePerUSD: number;
   priceHistory: number[];
@@ -9,19 +11,31 @@ type MarketActions = {
   setRandomDogePrice: () => void;
 };
 
-const defaultState: MarketState = {
-  dogePerUSD: Math.random() * 1000 + 1000,
-  priceHistory: [],
+const defaultState: () => MarketState = () => {
+  const storedLastPrice = localStorage.getItem(LAST_PRICE_KEY);
+  return {
+    dogePerUSD: storedLastPrice
+      ? parseInt(storedLastPrice)
+      : Math.random() * 1000 + 1000,
+    priceHistory: [],
+  };
 };
 
-const getRandomFluctuation = () => Math.random() * 500 - 250;
+const getRandomFluctuation = (currentVal: number) => {
+  const delta = currentVal / 2;
+
+  return Math.random() * currentVal - delta;
+};
 
 export const useMarketStorage = create<MarketState & MarketActions>((set) => ({
-  ...defaultState,
+  ...defaultState(),
   setRandomDogePrice: () => {
     set((state) => {
-      const price = state.dogePerUSD + getRandomFluctuation();
+      const price = state.dogePerUSD + getRandomFluctuation(state.dogePerUSD);
       const normalizedPrice = price > 0 ? price : 10;
+
+      localStorage.setItem(LAST_PRICE_KEY, normalizedPrice.toString());
+
       return {
         dogePerUSD: normalizedPrice,
         priceHistory: [...state.priceHistory, normalizedPrice],
