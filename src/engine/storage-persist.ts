4920e1c76de84156d7565e52219f3e2379572359
theo <me@t3.gg>
2021-04-30 22:52:21 -0700

    Remove console log

--- src/engine/storage-persist.ts
+++ src/engine/storage-persist.ts
@@ -5,7 +5,6 @@ import { GAME_STORAGE_KEY } from "./types";
 export const useStoragePersist = () => {
   const gameData = useGameStore();
   React.useEffect(() => {
-    console.log("updating storage");
     localStorage.setItem(GAME_STORAGE_KEY, JSON.stringify(gameData));
   }, [gameData.dogecoin, gameData.usd, gameData.dogePerUSD]);
 };
