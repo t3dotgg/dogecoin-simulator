68718bb9d6fcdcac13f5fc52a8b554ec49669997
theo <me@t3.gg>
2021-05-02 00:35:19 -0700

    Remove react-tooltip for now

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,11 @@ import "./index.css";
 import Game from "./Game";
 
 import ReactGA from "react-ga";
-import ReactTooltip from "react-tooltip";
 ReactGA.initialize("UA-25660758-13");
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <React.StrictMode>
-    <ReactTooltip />
-
     <Game />
   </React.StrictMode>,
   document.getElementById("root")
